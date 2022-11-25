c5d623be7412be39044f17b5e20cb667c7e516b2
高威威 <2950814933@qq.com>
2022-11-26 02:35:22 +0800

    '第二栏完成'

--- js/index.js
+++ js/index.js
@@ -93,3 +93,104 @@
     });
 
 })();
+// 全国用户总量统计
+
+(function () {
+    //自定义的柱状条
+    var item = {
+        name: '',
+        value: '1200',
+        itemStyle: {
+            color: "#254065"
+        },
+        tooltip: {
+            extraCssText: 'opacity:0'
+        }
+    }
+    var myChart = echarts.init(document.querySelector('.bar'));
+
+    var option = {
+        tooltip: {
+            trigger: 'item',
+        },
+        color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+                { offset: 0, color: '#00fffb' }, // 0 起始颜色
+                { offset: 1, color: '#0061ce' }  // 1 结束颜色
+            ],
+            global: false // 缺省为 false
+        },
+        grid: {
+            left: '0%',
+            right: '3%',
+            bottom: '3%',
+            top: '3%',
+            containLabel: true,
+            show: true,
+            // 宫格的颜色
+            borderColor: 'rgba(0, 240, 255, 0.3)'
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['郑州', '广州', '北京', '深圳', '合肥', '', '......', '', '杭州', '厦门', '济南', '成都', '重庆'],
+                axisTick: {
+                    alignWithLabel: false,
+                    show: false
+                },
+                axisLabel: {
+                    color: "#71f2fb"
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: 'rgba(0, 240, 255, 0.3)'
+                    }
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                axisLabel: {
+                    color: "#71f2fb"
+                },
+                axisTick: {
+                    show: false
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: 'rgba(0, 240, 255, 0.3)'
+                    }
+                },
+                splitLine: {
+                    lineStyle: {
+                        color: 'rgba(0, 240, 255, 0.3)'
+                    }
+                }
+            }
+        ],
+        series: [
+            {
+                name: 'Direct',
+                type: 'bar',
+                barWidth: '60%',
+                data: [2100, 1900, 1700, 1560, 1400, item, item, item, 900, 750, 600, 480, 240]
+            }
+        ]
+    }
+    myChart.setOption(option);
+    window.addEventListener("resize", function () {
+        // 让我们的图表调用 resize这个方法
+        myChart.resize();
+    });
+    //页面加载完成 就让图标自动重置大小
+    window.addEventListener("load", function () {
+        // 让我们的图表调用 resize这个方法
+        myChart.resize();
+    });
+})();
